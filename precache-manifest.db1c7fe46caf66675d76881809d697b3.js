4b34add3258aee4961b8bddd619dd5f6bacb459e
M Imam Pratama <imambungo@gmail.com>
2020-04-25 23:42:55 +0700

    Updates

---
+++ precache-manifest.db1c7fe46caf66675d76881809d697b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc100689b1820261a011c7cbb28433e",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "751c22c1da291443ee96",
+    "url": "/tic-tac-toe/static/css/main.bd4afd87.chunk.css"
+  },
+  {
+    "revision": "38c58a404a3fb46dd5f4",
+    "url": "/tic-tac-toe/static/js/2.b102e267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.b102e267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751c22c1da291443ee96",
+    "url": "/tic-tac-toe/static/js/main.1b034d7e.chunk.js"
+  },
+  {
+    "revision": "2e3e4e44d6a5c7256d12",
+    "url": "/tic-tac-toe/static/js/runtime-main.f4158612.js"
+  }
+]);
